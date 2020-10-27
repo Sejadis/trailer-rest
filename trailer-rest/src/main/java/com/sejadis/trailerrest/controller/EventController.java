5768fe5377a449e78ae33e6a2a2a7e467a86aaa6
Sejadis <sejadis-dev@gmx.de>
2020-10-27 16:09:38 +0100

    added endpoints to join/leave events

--- trailer-rest/src/main/java/com/sejadis/trailerrest/controller/EventController.java
+++ trailer-rest/src/main/java/com/sejadis/trailerrest/controller/EventController.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sejadis.trailerrest.entity.Club;
 import com.sejadis.trailerrest.entity.Event;
+import com.sejadis.trailerrest.entity.User;
 import com.sejadis.trailerrest.model.View;
 import com.sejadis.trailerrest.repository.ClubRepository;
 import com.sejadis.trailerrest.repository.EventRepository;
+import com.sejadis.trailerrest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @CrossOrigin
@@ -24,6 +27,8 @@ public class EventController {
     EventRepository eventRepository;
     @Autowired
     ClubRepository clubRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @PostMapping("/events")
     public @ResponseBody
@@ -46,6 +51,36 @@ public class EventController {
         }
         event.setDate(date);
         Event dbEvent = eventRepository.save(event);
+        return new ResponseEntity<>(dbEvent, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/events/{eventId}/users/{userId}")
+    public @ResponseBody
+    ResponseEntity<Event> addUser(@PathVariable long eventId, @PathVariable long userId) {
+        Optional<Event> event = eventRepository.findById(eventId);
+        Optional<User> user = userRepository.findById(userId);
+
+        if (event.isEmpty() || user.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        event.get().getUsers().add(user.get());
+        Event dbEvent = eventRepository.save(event.get());
+        return new ResponseEntity<>(dbEvent, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/events/{eventId}/users/{userId}")
+    public @ResponseBody
+    ResponseEntity<Event> removeUser(@PathVariable long eventId, @PathVariable long userId) {
+        Optional<Event> event = eventRepository.findById(eventId);
+        Optional<User> user = userRepository.findById(userId);
+
+        if (event.isEmpty() || user.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        Set<User> userSet = event.get().getUsers();
+        userSet.remove(user.get());
+        Event dbEvent = eventRepository.save(event.get());
         return new ResponseEntity<>(dbEvent, HttpStatus.CREATED);
     }
 
